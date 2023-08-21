de724b9da8d955be75a3ef615d786085b929a99c
emptyhead <korea6370@yu.ac.kr>
2023-08-21 23:42:43 +0900

    💜 [Add] : .gitignore 파일 추가

--- GoalForYou/Controller/LoginViewController.swift
+++ GoalForYou/Controller/LoginViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +24,15 @@ class LoginViewController: UIViewController {
     func configureUI() {
         backView.layer.cornerRadius = 20
         backView.clipsToBounds = true
+        
+        emailTextField.layer.cornerRadius = 10
+        emailTextField.clipsToBounds = true
+        
+        pwTextField.layer.cornerRadius = 10
+        pwTextField.clipsToBounds = true
+        
+        loginButton.layer.cornerRadius = 20
+        loginButton.clipsToBounds = true
     }
     
 
--- GoalForYou/Controller/RegisterViewController.swift
+++ GoalForYou/Controller/RegisterViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class RegisterViewController: UIViewController {
     
     @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +24,15 @@ class RegisterViewController: UIViewController {
     func configureUI() {
         backView.layer.cornerRadius = 20
         backView.clipsToBounds = true
+        
+        emailTextField.layer.cornerRadius = 10
+        emailTextField.clipsToBounds = true
+        
+        pwTextField.layer.cornerRadius = 10
+        pwTextField.clipsToBounds = true
+        
+        registerButton.layer.cornerRadius = 20
+        registerButton.clipsToBounds = true
     }
 
 }
